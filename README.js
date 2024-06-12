ca18c11f70fec177a7002e981be65e35bc2ae5b4
SadeQOA <163020784+SadeQOA@users.noreply.github.com>
2024-06-12 20:10:49 +0100

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="testy.css">
+</head>
+<body>
+    <div  class="container">
+        <input type="text" placeholder="Title" class="wide-input" id="id0">
+        
+        <div class="input-group">
+            <div class="hahah">
+                <input type="number" placeholder="Price" id="id1">
+                <input type="number" placeholder="Taxes" id="id2">
+                <input type="number" placeholder="Ads" id="id3" class="wide-input bigger-input">
+                <input type="number" placeholder="Discount" id="id4">
+                <input type="number" placeholder="Total:"  id="id5" disabled class="nigga smaller-input">
+            </div>
+        </div>
+
+        <input type="number" placeholder="Count" id="id9"><br>
+        <input type="text" placeholder="Category" id="id6"><br>
+        <button class="create-button" id="id8">Create</button>
+
+      
+        
+        <button class="delete-button" id="id10">Delete All</button>
+        
+       
+        <div id="id7"></div>
+        
+    </div>
+    <script src="test1.js"></script>
+</body>
+</html>
